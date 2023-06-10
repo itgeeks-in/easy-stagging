6e0fd800e77d35acb7666e53846e6a3a767a472e
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-06-10 15:36:17 +0530

    11 june13

--- web/frontend/pages/customers.jsx
+++ web/frontend/pages/customers.jsx
@@ -134,7 +134,20 @@ export default function Index() {
                         <input type="text" placeholder="Search by name, email, order" value={searchValue} onChange={changeSearchValue} name="search" />
                         <div className="itgOrderSearchButton">
                           <button onClick={searchData} type="button" className="btn primary-btn">
-                            <img src={searchIcon} alt="search" />
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                width="18" 
+                                height="18" 
+                                viewBox="0 0 24 24" 
+                                fill="none" 
+                                stroke="currentColor" 
+                                strokeWidth="2" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round" 
+                                className="feather feather-search">
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                            </svg>
                           </button>
                         </div>
                       </div>
