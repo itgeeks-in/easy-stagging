aa8b5854aa6480c6a51847287a02e75d60ec1c05
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-06-16 13:35:05 +0530

    15june 33

--- web/routes/web.php
+++ web/routes/web.php
@@ -387,7 +387,10 @@ Route::get('/api/payment', function (Request $request) {
                 $result = $client->query(['query' => $query]);
                 $data = $result->getDecodedBody();
                 $croninfo = DB::table('easylog')->insert([
-                    'data' => json_encode($data['data']['appSubscriptionCreate']['appSubscription'])
+                    'data' => $data['data']['appSubscriptionCreate']['appSubscription']['id']
+                ]);
+                $croninfo = DB::table('easylog')->insert([
+                    'data' => $data['data']['appSubscriptionCreate']['appSubscription']['confirmationUrl']
                 ]);
                 $application_charge_id = str_replace('gid://shopify/AppSubscription/','',$data['data']['appSubscriptionCreate']['appSubscription']['id']);
                 return response(json_encode(['status' => true, 'url' => $data['data']['appSubscriptionCreate']['appSubscription']['confirmationUrl'], 'id' =>$application_charge_id ]));
