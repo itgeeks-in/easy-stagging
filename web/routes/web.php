60ce5463a736b5c766572ff0a0fdc9d8fca8c31c
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-06-24 18:14:51 +0530

    24 june 6

--- web/routes/web.php
+++ web/routes/web.php
@@ -362,7 +362,6 @@ Route::get('/api/payment', function (Request $request) {
                             name: "Subscription"
                             returnUrl: "$returnUrl"
                             trialDays:15
-                            test:true
                             lineItems: [
                                 {
                                     plan: {
@@ -401,7 +400,6 @@ Route::get('/api/payment', function (Request $request) {
         }
         $application_charge->return_url = $returnUrl;
         $application_charge->trial_days = 15;
-        $application_charge->test = true;
         try {
             $application_charge->save(true);
             return response(json_encode(['status' => true, 'url' => $application_charge->confirmation_url, 'id' => $application_charge->id]));
