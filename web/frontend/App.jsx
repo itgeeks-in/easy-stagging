7f721d4e078bb28bd9ad5b02d21a58ddfa7abed5
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-07-13 16:50:17 +0530

    tawk

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -3,8 +3,6 @@ import { NavigationMenu } from "@shopify/app-bridge-react";
 import { ItgActivityContext } from "./context/activityContext";
 import Routes from "./Routes";
 
-import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
-
 import {
   AppBridgeProvider,
   QueryProvider,
@@ -52,9 +50,6 @@ export default function App() {
               />
               <Routes pages={pages} />
             </ItgActivityContext>
-            <TawkMessengerReact
-propertyId="64ae72afcc26a871b027eaa9"
-widgetId="1h54n00iv"/>
           </QueryProvider>
         </AppBridgeProvider>
       </BrowserRouter>
