b9f7ae20c1fff4f01c9b8a26136f14f4fc8bc27c
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-11-16 05:58:57 -0700

    Uploading

--- web/app/Http/Controllers/EasyAppCustomer.php
+++ web/app/Http/Controllers/EasyAppCustomer.php
@@ -34,19 +34,24 @@ class EasyAppCustomer extends Controller
             'data' => $allDataContent['signature']
         ]); 
 
-        if( isset( $allDataContent['shop'] ) && isset( $allDataContent['logged_in_customer_id'] ) && isset( $allDataContent['path_prefix'] ) && isset( $allDataContent['timestamp'] ) && isset( $allDataContent['signature'] ) ){
-
-            $croninfo = DB::table('easylog')->insert([
-                'data' => json_encode($allDataContent)
-            ]); 
-
-            // Return the data to the Shopify template.
-            return view('shopify.template', ['data' => '']);
-
-        }else{
-
+        if( is_null( $allDataContent['logged_in_customer_id'] ) ){
             return '';
-
+        }else{
+            if( isset( $allDataContent['shop'] ) && isset( $allDataContent['signature'] ) ){
+    
+                $croninfo = DB::table('easylog')->insert([
+                    'data' => json_encode($allDataContent)
+                ]); 
+    
+                // Return the data to the Shopify template.
+                return view('shopify.template', ['data' => '']);
+    
+            }else{
+    
+                return '';
+    
+            }
         }
+
     }
 }
