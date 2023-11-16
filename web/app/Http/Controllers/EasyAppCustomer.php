bff884397eb350d95fb8b685ed5a760cea32512b
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-11-16 05:50:04 -0700

    Data Upload 1234

--- web/app/Http/Controllers/EasyAppCustomer.php
+++ web/app/Http/Controllers/EasyAppCustomer.php
@@ -19,7 +19,19 @@ class EasyAppCustomer extends Controller
         $allDataContent = json_decode($allDataContent,true);
 
         $croninfo = DB::table('easylog')->insert([
-            'data' => json_encode($allDataContent)
+            'data' => $allDataContent['shop']
+        ]); 
+        $croninfo = DB::table('easylog')->insert([
+            'data' => $allDataContent['logged_in_customer_id']
+        ]); 
+        $croninfo = DB::table('easylog')->insert([
+            'data' => $allDataContent['path_prefix']
+        ]); 
+        $croninfo = DB::table('easylog')->insert([
+            'data' => $allDataContent['timestamp']
+        ]); 
+        $croninfo = DB::table('easylog')->insert([
+            'data' => $allDataContent['signature']
         ]); 
 
         if( isset( $allDataContent['shop'] ) && isset( $allDataContent['logged_in_customer_id'] ) && isset( $allDataContent['path_prefix'] ) && isset( $allDataContent['timestamp'] ) && isset( $allDataContent['signature'] ) ){
