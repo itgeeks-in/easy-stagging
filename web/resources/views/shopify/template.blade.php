7342c9719d8971f3c05a892fc974f86ad4874774
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-11-23 00:34:04 -0700

    DataFront One

--- web/resources/views/shopify/template.blade.php
+++ web/resources/views/shopify/template.blade.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manage Subscriptions</title>
-    <link rel="stylesheet" href="{{ asset('css/shopify.css') }}">
-</head>
-<body>
-<div class="easySubscriptionWidget">
-  <div class="easySubscriptionWidgetIn">
-      <div class="easySubscriptionWidgetModel" style="display:none;">
-          <div class="easySubscriptionWidgetModelInner">
-            <div class="easySubscriptionWidgetModelInnerTitle">
-                <h3 class="title">Subscriptions</h3>
-                <button type="button" class="btn button" id="easySubscriptionClosebtn">Close</button>
-            </div>
-            <div id="easySubscriptionWidgetModelIn" class="easySubscriptionWidgetModelIn">
-                <div class="easySubscriptionWidgetModelInstatusfilters">
-                    <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter active" value='All'>All</span>
-                    <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Active">Active</span>
-                    <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Paused">Paused</span>
-                    <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Cancelled">Cancelled</span>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Manage Subscriptions</title>
+        <link rel="stylesheet" href="{{ asset('css/shopify.css') }}">
+    </head>
+    <body>
+        <div class="easySubscriptionWidget">
+            <div class="easySubscriptionWidgetIn">
+                <div class="easySubscriptionWidgetModel" style="display:block;">
+                    <div class="easySubscriptionWidgetModelInner">
+                        <div class="easySubscriptionWidgetModelInnerTitle">
+                            <h3 class="title">Subscriptions</h3>
+                            <button type="button" class="btn button" id="easySubscriptionClosebtn">Close</button>
+                        </div>
+                        <div id="easySubscriptionWidgetModelIn" class="easySubscriptionWidgetModelIn">
+                            <div class="easySubscriptionWidgetModelInstatusfilters">
+                                <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter active" value='All'>All</span>
+                                <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Active">Active</span>
+                                <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Paused">Paused</span>
+                                <span class="easySubscriptionlinks easySubscriptionlinksstatusfilter" value="Cancelled">Cancelled</span>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div id="easySubscriptionSubscriptionModel" style="display:none;" class="easySubscriptionSubscriptionModel">
+                    <div id="easySubscriptionSubscriptionModelInner" class="easySubscriptionSubscriptionModelInner">
+                    <div class="easySubscriptionWidgetModelInnerTitle">
+                        <h3 class="title">Subscription <span id="easySubscriptionId"></span></h3>
+                        <button type="button" class="btn button" id="easySubscriptionInnerClosebtn">Close</button>
+                    </div>
+                    </div>
                 </div>
             </div>
-          </div>
-      </div> 
-      <div id="easySubscriptionSubscriptionModel" style="display:none;" class="easySubscriptionSubscriptionModel">
-        <div id="easySubscriptionSubscriptionModelInner" class="easySubscriptionSubscriptionModelInner">
-          <div class="easySubscriptionWidgetModelInnerTitle">
-            <h3 class="title">Subscription <span id="easySubscriptionId"></span></h3>
-            <button type="button" class="btn button" id="easySubscriptionInnerClosebtn">Close</button>
-          </div>
         </div>
-      </div>
-  </div>
-</div>
-</body>
+    </body>
 </html>
