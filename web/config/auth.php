4f608cd0216f048938f7ec3df9f31cc679f5235b
IT GEEKS <itgeeks@ITs-MacBook-Pro.local>
2023-12-04 06:00:55 -0700

    JWT authetication 12345678

--- web/config/auth.php
+++ web/config/auth.php
@@ -36,17 +36,11 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-    
-        'api' => [
-            'driver' => 'jwt',
-            'provider' => 'users',
-        ],
+//        'web' => [
+//            'driver' => 'session',
+//            'provider' => 'users',
+//        ],
     ],
-    
 
     /*
     |--------------------------------------------------------------------------
