9eb0f7430d490f61cc45208eaa49cde9e56a0d78
IT GEEKS <itgeeks@MacBook-Pro.local>
2024-01-08 00:09:58 -0700

    Dunning Upload

--- web/frontend/pages/dunning.jsx
+++ web/frontend/pages/dunning.jsx
@@ -205,6 +205,7 @@ export default function NotificationSettings(){
                                     {updated?<>
                                         <div class="itgDashboardAction">Settings saved successfully.</div>
                                     </>:<></>}
+                                    <div class="itgDashboardAction">Settings saved successfully.</div>
                                 </div>
                             </div>
                         </div>
